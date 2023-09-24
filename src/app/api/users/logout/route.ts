a3f688632d8686e1ea11bd5f0614230556000e5b
ibadurrahmaAlHadi <extremegilang@gmail.com>
2023-09-25 06:21:50 +0700

    hopefully work

--- src/app/api/users/logout/route.ts
+++ src/app/api/users/logout/route.ts
@@ -1,6 +1,6 @@
-import { NextResponse } from "next/server";
+import { NextResponse,  NextRequest } from "next/server";
 
-export async function GET(){
+export async function GET(request : NextRequest){
     try {
         
         const response = await NextResponse.json({
