723043962ba0d3f2d67281ad310e991ba5f5f372
ibadurrahmaAlHadi <extremegilang@gmail.com>
2023-09-25 05:52:25 +0700

    changing logout button

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -34,7 +34,7 @@ function Navbar() {
     <div className=" shadow-md bg-white">
       <div className="flex justify-between items-center h-[70px] max-w-[1240px] m-auto px-5 xl:px-0">
         <div className="flex text-center flex-1 font-bold gap-6 items-center">
-          <Image src='/logo.png' alt="" width={30} height={30} />
+          <Image src='/logo.svg' alt="" width={30} height={30} />
           <h1 className="text-[20px]">PR</h1>
           <Link href='/progress/skripsi'>Progress Skripsi</Link>
           <Link href='/progress/sempro'>Progress Sempro</Link>
@@ -43,7 +43,7 @@ function Navbar() {
           {/* {data && (
             <Link href={`/progress/profile/${data}`} >{data}</Link>
           )} */}
-          <span onClick={handleLogout} className="cursor-pointer hover:text-green-500 duration-500">Logout</span>
+          <button onClick={handleLogout} className="cursor-pointer hover:text-green-500 duration-500">Logout</button>
         </div>
       </div>
     </div>
